01c4516d42114f0c070fd7b5906aa4ef51f2caa9
seunghee114 <201802069@o.cnu.ac.kr>
2018-11-23 19:59:07 +0900

    no message

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,13 @@
 public class Calculator{
+
+  public void main (String[] args){
+    int a = 4;
+    int b = 2;
+    System.out.println(sum(a,b))
+  }
+
   public int sum(int a, int b){
     return a + b;
+
   }
 }
